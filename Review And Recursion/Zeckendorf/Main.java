c17bd77a79793483b4409afe680d299d1f3375bf
DavidSantosTXUSA <71650341+DavidSantosTXUSA@users.noreply.github.com>
2020-12-17 15:19:30 -0600

    Create Main.java

---
+++ Review And Recursion/Zeckendorf/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+/**
+ * The only thing that can be modified here is the file name.
+ */
+
+class Main 
+{
+  public static void main(String[] args) throws IOException
+  {
+    Zeckendorf test = new Zeckendorf();
+		test.zeck("zeck2.txt");		
+  }
+}
